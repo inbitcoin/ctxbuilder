8916c7f9a95bb01ebc0da529e3096bf4ed8fac43
Martino Salvetti <tldr@inbitcoin.it>
2019-10-28 11:38:56 +0100

    Fix type of the constructor of ColoredCoinsBuilder

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 declare module '@inbitcoin/ctxbuilder' {
   class ColoredCoinsBuilder {
-    constructor(param?: { network: 'mainnet' | 'testnet' })
+    constructor(param?: { network: "mainnet" | "testnet" | "regtest"; mindustvalue?: number; softMaxUtxos?: number; })
 
     public buildSendTransaction(args: any): ICapiBuiltTransaction
     public buildIssueTransaction(args: any): ICapiBuiltIssueTransaction
